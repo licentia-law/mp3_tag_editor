8266e460225763ed38d8eda9a9e87569f1f118d2
Law <mychosh@gmail.com>
2023-06-13 10:37:17 +0900

    ADD annotation

--- mp3_tag_editor.py
+++ mp3_tag_editor.py
@@ -2,18 +2,20 @@
 
 import eyed3
 
-# 곡 정보 입력
-mp3 = "Tesla - Love Song.mp3"
-artist = "Tesla"
-title = "Love Song"
+### 곡 정보 입력
+mp3 = "Tesla - Love Song.mp3" # 파일명, .mp3도 같이 기입
+artist = "Tesla" # 아티스트, 참여 아티스트
+title = "Love Song" # 곡 제목
+# album = "Album"
+# track_num = 1 # 트랙 넘버
 
-# 곡 정보 수정
-audiofile = eyed3.load(mp3)
-audiofile.tag.artist = artist
-# audiofile.tag.album = "Free For All Comp LP"
-audiofile.tag.album_artist = artist
-audiofile.tag.title = title
-# audiofile.tag.track_num = 3
+### 곡 정보 수정
+audiofile = eyed3.load(mp3) # 곡 경로
+audiofile.tag.artist = artist # 아티스트
+audiofile.tag.album_artist = artist # 앨범 참여 아티스트
+audiofile.tag.title = title # 곡 제목
+# audiofile.tag.album = album # 앨범
+# audiofile.tag.track_num = track_num # 트랙 넘버
 
-# 곡 정보 저장
+### 곡 정보 저장
 audiofile.tag.save()
